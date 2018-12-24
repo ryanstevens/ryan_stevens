c809ab9e010c5bc146ae270cff029ca0ae2f72bc
Ryan Stevens <stevens.ryan@gmail.com>
2018-12-24 09:48:38 -0800

    haha, making it actually work

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 const chalk = require('chalk');
 const yel = chalk.yellow.bind(chalk);
 const red = chalk.red.bind(chalk);
-const bol = chalk.bol.bind(chalk);
+const bol = chalk.bold.bind(chalk);
+const whi = chalk.white.bind(chalk);
 
 console.log(`
 
-${yel(`*********************************************************`)}
-${yel(`**                                                     **`)}
-${yel(`**                                                     **`)}
-${yel(`**               ${bol(`Ryan Stevens`)}                                      **`)}
-${yel(`**                                                     **`)}
-${yel(`**                                                     **`)}
-${yel(`*********************************************************`)}
+${yel(`***********************************************`)}
+${yel(`**                                           **`)}
+${yel(`**                                           **`)}
+${yel(`**               ${whi(bol(`Ryan Stevens`))}                **`)}
+${yel(`**                                           **`)}
+${yel(`**                                           **`)}
+${yel(`***********************************************`)}
 
 
 
